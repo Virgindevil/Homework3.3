b797f3d57a73bfca92446fb6b7bf90ccc769abc4
S.Matuzov <98310589+Virgindevil@users.noreply.github.com>
2022-01-27 15:20:47 +0400

    Add files via upload

---
+++ Homework3.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Homework3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Chet = true;
+            int i = 2;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Проверим ваше число, простое ли оно!");
+            Console.WriteLine("Введите число:");
+            int number = int.Parse(Console.ReadLine());
+            while (i <= number / 2)
+            {
+                if (number % i == 0)
+                {
+                    Chet = false;
+                    break;
+                }
+                i++;
+            }
+            if (Chet == true)
+            {
+                Console.WriteLine("Число простое");
+            }
+            else
+            {
+                Console.WriteLine("Число не простое");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
